251471f5eb9f279834ad86905527d84de3edaf18
marsim0 <marb@mail.ru>
2015-05-24 17:35:43 +0300

    Initial Commit

---
+++ core_data/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  core_data
+//
+//  Created by Мариам Б. on 24.05.15.
+//  Copyright (c) 2015 Мариам Б. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
